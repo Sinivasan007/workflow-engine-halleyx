de11e3e4625f8d31200c92b9d21a8f8d3d4e499b
Sinivasan S <sinivasans157@gmail.com>
2026-05-21 21:00:53 +0530

    Add SSL configuration for database connection

--- backend/setup_db.js
+++ backend/setup_db.js
@@ -7,6 +7,9 @@ async function setup() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+    rejectUnauthorized: false,
+  },
   });
 
   try {
